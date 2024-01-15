d7140412b83f2c29806f07ff6776d3769a10d685
without eyes <37347347+without-eyes@users.noreply.github.com>
2024-01-15 20:09:07 +0200

    dasmiawfkmamkafwmkafmkafekm

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,13 +1,1 @@
 extends Node2D
-
-var test_array: Array[String] = ["Test", "hello", "WORLD"]
-
-func _ready():
-	$Logo.rotation_degrees = 90
-
-func _process(delta):
-	$Logo.rotation_degrees += 200 * delta
-		
-	if $Logo.position.x > 1000:
-		$Logo.pos.x = 0
-		
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,6 +1,13 @@
-extends Node2D
+extends CharacterBody2D
 
 
-func _process(delta):
+func _process(_delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
-	position += direction * 500 * delta
+	velocity = direction * 500
+	move_and_slide()
+	
+	if Input.is_action_pressed("primary action"):
+		print('shoot laser')
+		
+	if Input.is_action_pressed("secondary action"):
+		print('shoot granade')
