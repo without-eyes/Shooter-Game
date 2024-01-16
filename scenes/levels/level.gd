7fa3e079b33168e1806b9819ddb3ada9ad744ec4
without eyes <37347347+without-eyes@users.noreply.github.com>
2024-01-16 20:46:28 +0200

    dsasdsadasdasd

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,16 +1,17 @@
 extends Node2D
 
-
+var laser_scene: PackedScene = preload("res://scenes/projectiles/laser.tscn")
 
 func _on_gate_player_entered_gate(body):
 	print('player has entered gate')
 	print(body)
+	
 
 func _on_player_laser():
-	print('laser from level')
+	var laser = laser_scene.instantiate()
+	laser.position = $Player.position
+	add_child(laser)
+	
 
 func _on_player_granade():
 	print('granade from level')
-
-
-
