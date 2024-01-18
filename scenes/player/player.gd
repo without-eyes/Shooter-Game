4d795585f9762735bfdd665f51007ae2e296bf76
without eyes <37347347+without-eyes@users.noreply.github.com>
2024-01-18 17:54:57 +0200

    dsadsadsadsadsaasd

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,27 +1,32 @@
 extends CharacterBody2D
 
+signal laser(pos, direction)
+signal granade(pos, direction)
+
 var can_laser: bool = true
 var can_granade: bool = true
-
-signal laser
-signal granade
 
 func _process(_delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * 500
 	move_and_slide()
 	
+	look_at(get_global_mouse_position())
+	
+	var player_direction = (get_global_mouse_position() - position).normalized()
 	if Input.is_action_pressed("primary action") and can_laser:
-		print('shoot laser')
+		var laser_markers = $LaserStartPositions.get_children()
+		var selected_laser = laser_markers[randi() % laser_markers.size()]
 		can_laser = false
 		$LaserReloadTimer.start()
-		laser.emit()
+		laser.emit(selected_laser.global_position, player_direction)
 		
 	if Input.is_action_pressed("secondary action") and can_granade:
-		print('shoot granade')
+		var selected_granade = $LaserStartPositions.get_children()[0]
+		
 		can_granade = false
 		$GranadeReloadTimer.start()
-		granade.emit()
+		granade.emit(selected_granade.global_position, player_direction)
 
 
 func _on_timer_timeout():
